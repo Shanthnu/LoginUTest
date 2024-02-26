209243b4d4f08fa126cf5a3ed7e1a6d086ccd480
Shanth <Shanthu7318@gmail.com>
2024-02-26 16:21:52 +0530

    Initail Commit

---
+++ SGLoginUTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SGLoginUTest
+//
+//  Created by Santhosh on 26/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
